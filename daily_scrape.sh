c9f83bae468240e46f1e2418992a9aac63dfbd7e
gabriel1200 <gabe.r.guzman@gmail.com>
2025-01-31 11:55:01 -0600

    quick commit

--- daily_scrape.sh
+++ daily_scrape.sh
@@ -1,21 +1,39 @@
-jupyter nbconvert --to script game_report/game_report_scrape.ipynb
+#!/bin/bash
 
-jupyter nbconvert --to script game_report/pbp_gamelogs.ipynb
+# Convert Jupyter notebooks to Python scripts in their respective directories
+cd game_report
+jupyter nbconvert --to script game_report_scrape.ipynb
+jupyter nbconvert --to script pbp_gamelogs.ipynb
+cd ..
 
+cd team_totals
+jupyter nbconvert --to script team_average_scrape.ipynb
+cd ..
 
-jupyter nbconvert --to script team_totals/team_average_scrape.ipynb
-jupyter nbconvert --to script gen_totals/averages_scrape.ipynb
-jupyter nbconvert --to script gen_totals/gen_totals.ipynb
+cd gen_totals
+jupyter nbconvert --to script averages_scrape.ipynb
+jupyter nbconvert --to script gen_totals.ipynb
+cd ..
 
-jupyter nbconvert --to script lineups/lineups.ipynb
+cd lineups
+jupyter nbconvert --to script lineups.ipynb
+cd ..
 
+# Run the Python scripts in their respective directories
+cd game_report
+python game_report_scrape.py
+python pbp_gamelogs.py
+cd ..
 
+cd team_totals
+python team_average_scrape.py
+cd ..
 
-python game_report/game_report_scrape.py
+cd gen_totals
+python averages_scrape.py
+python gen_totals.py
+cd ..
 
-python game_report/pbp_gamelogs.py
-python team_totals/team_average_scrape.py
-python gen_totals/averages_scrape.py
-python gen_totals/gen_totals.py
-
-python lineups/lineups.py
+cd lineups
+python lineups.py
+cd ..
